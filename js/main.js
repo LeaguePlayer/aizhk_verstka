6e2303354d988b9c972ac92759276f44a7f0f170
DiggerZ <DiggerZ@mail.ru>
2013-11-06 19:00:45 +0600

    top

--- js/main.js
+++ js/main.js
@@ -58,20 +58,42 @@ $(document).ready(function(){
 
 	function fixSteps(top, step){
 		if(top>step) {
-			$('.top_steps').css({'position': 'fixed', 'background-color': '#fff', 'box-shadow': '10px 10px 10px #fff'}).animate({'top': 0}, 100);
+			$('.top_steps').css({'position': 'fixed', 'background-color': '#fff', 'box-shadow': '10px 15px 20px #fff'}).animate({'top': 0}, 100);
 			$('.steps_wrap .shadow').fadeIn(1000);
+			tipsResize(top, step);
 		} else {		
 			$('.top_steps').css({'position': 'static', 'background-color': '#f0f4f9', 'box-shadow': 'none'});
 			$('.steps_wrap .shadow').fadeIn(2000);
 		}
 	}
 
+	function tipsResize(top, step) {
+		var range = top-step;
+		var rangeLow = 180 - range;
+		var fontSize = rangeLow/10;
+		if(fontSize>=12) {
+			$('.progress p').stop(true, false).css({'font-size': fontSize, 'background-position': 'center '+fontSize*2.1+'px', 'background-size': fontSize*1.777777777777778});
+			$('.progress').stop(false, false).css({'width': fontSize*45.55555555555556, 'padding-top': fontSize*0.5555555555555556, 'height': fontSize*3+'px'});
+			$('.top_steps').css({'height': fontSize*5+'px'})
+			$('.progress .step4').css({'background-size': fontSize*3.444444444444444+'px '+fontSize*2.222222222222222+'px', 'right': fontSize*-3.8+'px', 'background-position': 'center '+fontSize*1.72+'px'});
+			$('.progress .step2').css({'left': fontSize*7.5});
+			$('.progress .step3').css({'left': fontSize*26.66666666666667});
+		}
+
+		if(fontSize>17) {
+			$('.progress p').attr('style', '');
+			$('.top_steps').attr('style', '');
+			$('.progress').attr('style', '');
+		}
+		
+	}	
+
 	$(window).scroll(function(){
 
 		var winTop = $(this).scrollTop();
 		var stepsTop = $('.steps_wrap').offset().top;
 
-		fixSteps(winTop, stepsTop);
+		fixSteps(winTop, stepsTop); 
 
 	});
 
