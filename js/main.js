2f760d1ac93c5efbfe83eb5bed7e62c61343cea6
DiggerZ <DiggerZ@mail.ru>
2013-11-06 16:16:16 +0600

    aijk

---
+++ js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+	/* /// Разделение суммы на тысячи */
+		function format(comma, period) {
+		  comma = comma || ',';
+		  period = period || '.';
+		  var split = this.toString().split('.');
+		  var numeric = split[0];
+		  var decimal = split.length > 1 ? period + split[1] : '';
+		  var reg = /(\d+)(\d{3})/;
+		  while (reg.test(numeric)) {
+		    numeric = numeric.replace(reg, '$1' + comma + '$2');
+		  }
+		  return numeric + decimal;
+		}
+
+		$('.inp').on('keyup', function(){
+		    $(this).val(format.call($(this).val().split(' ').join(''),' ','.'));
+		});
+	/* Разделение суммы на тысячи /// */
+
+	$('.what_interest .item').click(function(){
+		
+		var targetId = $(this).index()-1;
+		
+		if(!$(this).is('.active')) {
+
+			movePointer(targetId);
+			showPage(targetId);
+		}
+		return false;
+	});
+
+	function movePointer(id) {
+
+		var leftPos = $('.what_interest .item').eq(id).position().left;
+		var width = $('.what_interest .item').eq(id).outerWidth()-$('.what_interest .item').eq(id).width()+($('.what_interest .item').eq(id).width()/2)+leftPos-30;
+
+		$('.pointer_rail .pointer').animate({left: width}, 150, function(){
+			$('.what_interest .item.active').removeClass('active')/*.css({'font-weight': '300'})*/;
+			$('.what_interest .item').eq(id).addClass('active')/*.css({'font-weight': 'bold'})*/;
+		});
+	}
+
+	function showPage(id){
+
+		var newHeight = $('.calculate .calc').eq(id-1).height();
+		var top = $('body, html').scrollTop();
+		$('.calculate .calc:visible').stop(true, true).fadeOut(500);
+		$('.calculate .calc').eq(id-1).stop(true, true).fadeIn(500);
+		$('.calculate').animate({'height': newHeight});
+		/*$('body, html').animate({scrollTop: 0}, 500);*/
+	}
+
+	showPage(1);
+	movePointer(0);
+
+
+	function fixSteps(top, step){
+		if(top>step) {
+			$('.top_steps').css({'position': 'fixed', 'background-color': '#fff', 'box-shadow': '10px 10px 10px #fff'}).animate({'top': 0}, 100);
+			$('.steps_wrap .shadow').fadeIn(1000);
+		} else {		
+			$('.top_steps').css({'position': 'static', 'background-color': '#f0f4f9', 'box-shadow': 'none'});
+			$('.steps_wrap .shadow').fadeIn(2000);
+		}
+	}
+
+	$(window).scroll(function(){
+
+		var winTop = $(this).scrollTop();
+		var stepsTop = $('.steps_wrap').offset().top;
+
+		fixSteps(winTop, stepsTop);
+
+	});
+
+});
---
+++ js/spinner/jquery.spinnercontrol.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function(){
+
+    var startNumber = 120;
+    var sid;
+
+    $('.price_slider').each(function(){
+
+        sid++;
+
+        $(this).noUiSlider({
+             range: [0,150]
+            ,start: [startNumber]
+            ,handles: 1
+            ,step: 1
+            ,slide: function(){
+                $('.calc').each(function(){
+                    $(this).find('.ValueDisplay').val(Math.round($(this).find('.price_slider').val(), 2));
+                });
+            }
+        });
+    });
+
+    $('.SpinnerControl').each(function(){
+
+        $(this).find('.ValueDisplay').val(startNumber);
+
+        $(this).find('.RightButton').click(function(){
+            var current = parseInt($(this).parent().find('.ValueDisplay').val());
+            $(this).parent().find('.ValueDisplay').val(current+1);
+             $(this).find('.price_slider').val(current+1);
+        });
+
+        $(this).find('.LeftButton').click(function(){
+            var current = parseInt($(this).parent().find('.ValueDisplay').val());
+            $(this).parent().find('.ValueDisplay').val(current-1);
+            $(this).find('.price_slider').val(current-1);
+        });
+
+    });
+    
+
+});
